78d9f5a101e01a7f9eae447ea44073b53816c07c
IvanM6767 <vanioivamami@gmail.com>
2023-03-09 22:45:20 +0300

    5sprr

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -107,14 +107,17 @@ class PostFormTests(TestCase):
 
     def test_create_post_not_authorized(self):
         """Тестирование создания поста не авторизированным пользователем"""
-        post_count = Post.objects.all().count()
+        post_count = Post.objects.count()
         form_data = {
-            'text': 'Текст поста',
-            'group': self.group_1.id
+            'group': self.group_1.id,
+            'text': 'Тестовый текст',
         }
+        # Убедился что пост один в базе, до создания еще одного.
+        self.assertEqual(Post.objects.count(), 1)
         response = self.client.post(
             reverse('posts:post_create'), data=form_data
         )
-        self.assertEqual(response.status_code, 200)
-        # Убедился что пост один в базе, до создания еще одного.
+        # Убедился что количество постов не изменилось
         self.assertEqual(Post.objects.count(), post_count)
+        # Убедился что не авторизованный получил редирект
+        self.assertEqual(response.status_code, 302)
